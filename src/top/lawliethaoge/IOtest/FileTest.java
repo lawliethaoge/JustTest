9ea37fb1ec113adb04801294aba85e94e8453fa3
lawliet <653772348@qq.com>
2018-05-24 11:34:07 +0800

    IOtest1

--- src/top/lawliethaoge/IOtest/FileTest.java
+++ src/top/lawliethaoge/IOtest/FileTest.java
@@ -1,11 +1,36 @@
 package top.lawliethaoge.IOtest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 /**
  * @author hao@lawliet.com
  * @since 2018/5/24 11:04
  */
 public class FileTest {
-    public static void main(String[] args) {
-        
+    /**
+     * 展示E盘里的所有文件夹
+     * @param []
+     * @return void
+     */
+    public static void ShowFileName(){
+        File file = new File("E:\\");
+        File[] files = file.listFiles();
+        for(File file1 : files){
+            System.out.println(file1.getName());
+        }
+
+
+
+    }
+    public static void main(String[] args) throws Exception {
+        File file = new File("E:\\haha.txt");
+        InputStream inputStream = new FileInputStream(file);
+        byte[] bytes = new byte[(int)file.length()];
+        inputStream.read(bytes);
+        System.out.println(new String(bytes));
+        ShowFileName();
     }
 }
