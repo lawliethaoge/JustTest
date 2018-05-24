da547b17346c62b394673ae8d02acd27f9d4a553
lawliet <653772348@qq.com>
2018-05-24 11:10:31 +0800

    first

---
+++ src/top/lawliethaoge/IOtest/FileTest.java
@@ -0,0 +1,11 @@
+package top.lawliethaoge.IOtest;
+
+/**
+ * @author hao@lawliet.com
+ * @since 2018/5/24 11:04
+ */
+public class FileTest {
+    public static void main(String[] args) {
+        
+    }
+}
