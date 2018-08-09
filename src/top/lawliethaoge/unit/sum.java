b77e3df70676393f064dfa724065d0fd2a353861
lawliet <cz653772348@163.com>
2018-08-09 17:56:25 +0800

    test1

--- src/top/lawliethaoge/unit/sum.java
+++ src/top/lawliethaoge/unit/sum.java
@@ -7,10 +7,24 @@ package top.lawliethaoge.unit;
 public class sum {
     public static void main(String[] args) {
         int sum = 0;
-        int  a[] = {
+        int[] a;
+        a = new int[]{1,
+                3,
+                3,
+                3,
+                3,
+                3,
+                3,
+                4,
+                4,
+                4,
+                5,
+                6,
+                6,
+                7
         };
-        for(int i : a){
-            sum+= i;
+        for (int i : a) {
+            sum += i;
         }
         System.out.println(sum);
     }
